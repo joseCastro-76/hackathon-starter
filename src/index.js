474698d46eb7a1c88ca5c99972ad4df9d62cd8bc
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-04-17 21:51:49 -0700

    React app that displays covid stats and graph styled with Material UI. Nav component implemented.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import './css/custom.scss';
 
-render(<h1 className='red'>Hello, World</h1>, document.getElementById('root'));
+// import Button from '@material-ui/core/Button';
+
+import FullWidthTabs from './fullWidthTabs';
+
+function App() {
+    return (
+        <FullWidthTabs />
+    )
+}
+
+render(<App />, document.getElementById('root'));
